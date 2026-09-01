30098600e19d17bb1ff529f52c9afdaca4e89cef
Mohammed <you@example.com>
2026-09-01 18:46:04 +0300

    file processing completed

--- src/routes/data.py
+++ src/routes/data.py
@@ -7,6 +7,8 @@ from controllers import DataController, ProjectController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
+from controllers import ProceseController
 logger=logging.getLogger("uvicorn.error")
 
 data_router = APIRouter(
@@ -57,3 +59,23 @@ async def upload_file(
             "file id" :file_id
         }
     )
+@data_router.post("/process/{project_id}") 
+async def process_endpoint(project_id: str,process: ProcessRequest,):
+    file_id = process.file_id
+    chunk_size = process.chunk_size
+    overlap_size = process.overlap_size
+    process_controller = ProceseController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id)
+    file_chunks = process_controller.process_content(file_content=file_content,
+                                                    file_id=file_id,
+                                                    chunk_size=chunk_size,
+                                                    overlap_size=overlap_size)
+    if file_chunks is None or len(file_chunks)==0 :
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignal.PROCESSING_FAILED.value
+                }
+            )
+    return file_chunks
